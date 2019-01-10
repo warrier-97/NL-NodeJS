c9b7a167c8fdadc4637fb09fa27f4387e8950fb8
Prashanth Puranik <puranik@digdeeper.in>
2019-01-10 10:08:22 +0530

    serving products and reviews

--- puranik3/01-intro/01-simple-server.js
+++ puranik3/01-intro/01-simple-server.js
@@ -1,12 +1,28 @@
 const http = require( 'http' );
+const products = require( './data/products.json' );
+const reviews = require( './data/reviews.json' );
 
 // the callback function is called when the server receives an HTTP rquest
+// npm install -g nodemon
 const server = http.createServer(function( req, res ) {
     console.log( req.url )
-    res.end( 'hello world' );
+
+    switch( req.url ) {
+        case '/products':
+            res.end( JSON.stringify( products ) );
+            break;
+        case '/reviews':
+            res.end( JSON.stringify( reviews ) );
+            break;
+        case '/feeback':
+            res.end( 'feedback will be sent' );
+            break;
+        default:
+            res.end( 'hello world' );
+    }
 });
 
-server.listen(8080, '127.0.0.1', function( error ) {
+server.listen(8080, function( error ) {
     if( error ) {
         console.log( 'server could not be started | error = ' + error.message );
         return;
