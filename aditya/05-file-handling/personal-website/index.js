6ecb658f3b4a4dd2ee806cdc5e0df6f20552821c
aditya <aditya@nineleaps.com>
2019-01-10 17:47:26 +0530

    Personal website

--- aditya/05-file-handling/personal-website/index.js
+++ aditya/05-file-handling/personal-website/index.js
@@ -13,6 +13,7 @@ const fs = require('fs')
 // })
 
 const http = require('http')
+const anyBody = require('body/any')
 const server = http.createServer()
 function requestHandler(req,res){
     res.setHeader('Content-Type','text/html')
@@ -35,6 +36,23 @@ function requestHandler(req,res){
             res.end(data)
             })
             break;
+
+        case '/save':
+            if(req.method.toUpperCase() !== 'POST'){
+                res.end('Invalid HTTP method. Use POST /save to send data')
+            }
+            else{
+                anyBody(req,res,{},function(error,contact){
+                    if(error){
+                        res.end("Sorry! Will get back Soon!")
+                        return;
+                    }
+
+                    fs.writeFile(`./contacts/${contact.email.txt}`)
+                    res.end(`Hey ${contact.name}! Will contact you soon on ${contact.email}`)
+                })
+            }
+        
         default :
             fs.readFile('./page-not-found.html',"utf8",function(error,data){
             if(error){
