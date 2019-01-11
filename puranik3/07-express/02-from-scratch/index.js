b93ca8e5bbc5134b5c60fad749a651496b373a75
Prashanth Puranik <puranik@digdeeper.in>
2019-01-11 13:15:20 +0530

    minor edit

--- puranik3/07-express/02-from-scratch/index.js
+++ puranik3/07-express/02-from-scratch/index.js
@@ -1,4 +1,7 @@
 const express = require( 'express' );
+const contacts = require( './contacts.json' );
+
+console.log( __filename, __dirname );
 
 /* App is an Express Application object - also a web server */
 const app = express();
@@ -6,6 +9,12 @@ const app = express();
 // Handle GET /
 app.get( '/', function( req, res ) {
     res.end( 'Hello world' );
+});
+
+// Handle GET /contacts and return contents of contact.json file
+app.get( '/contacts', function( req, res ) {
+    // res.sendFile( __dirname + '/contacts.json' );
+    res.json( contacts );
 });
 
 app.listen( 8080, function( error ) {
