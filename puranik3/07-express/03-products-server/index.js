619ed1aed771c111cc4fdb117000982c754a6002
Prashanth Puranik <puranik@digdeeper.in>
2019-01-12 11:07:18 +0530

    added custom middleware

--- puranik3/07-express/03-products-server/index.js
+++ puranik3/07-express/03-products-server/index.js
@@ -2,7 +2,22 @@ const express = require( 'express' );
 const indexRouter = require( './routers/index' );
 const productsRouter = require( './routers/products' );
 const reviewsRouter = require( './routers/reviews' );
+const chalk = require( 'chalk' );
+
 const app = express();
+
+app.use(function( req, res, next ) {
+    req.receivedAt = new Date();
+    
+    // pass control to the next middleware - without this your server will not send response
+    next();
+});
+
+// Exercise: Write a middleware that logs receivedAt to the console
+app.use(function( req, res, next ) {
+    console.log( chalk.red( 'Request was received at ' + req.receivedAt ) );
+    next();
+});
 
 // middleware functions
 // executed in order in which they are set
